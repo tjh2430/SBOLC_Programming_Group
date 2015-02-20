6ec975c2e021abe2cd9873496c682fe19d4a952d
theard <TimHeard@hotmail.com>
2015-02-20 14:16:24 -0500

    Reorganzied source directories

---
+++ group_project/RiskProject/RiskApp/src/main/GameInstance.py
@@ -0,0 +1,6 @@
+
+#
+# Represents a single instance of a Risk game (i.e. a game that is being
+# played by a group of players).
+#
+class GameInstance:
---
+++ group_project/RiskProject/RiskApp/src/rulesets/BaseRuleSet.py
@@ -0,0 +1,8 @@
+from abc import ABCMeta
+
+class BaseRuleSet:
+    __metaclass__ = ABCMeta
+
+    @abstactmethod
+    def 
+
